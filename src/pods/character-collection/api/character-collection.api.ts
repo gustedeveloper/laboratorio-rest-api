4c1851013b42cfdb364b4c66cbddc20b1a348cca
Guste Gaubaite <219.guste@gmail.com>
2025-07-07 11:04:47 +0200

    feat: convert character collection API from REST to GraphQL

--- src/pods/character-collection/api/character-collection.api.ts
+++ src/pods/character-collection/api/character-collection.api.ts
@@ -1,7 +1,42 @@
 import { Character } from './character-collection.api-model';
+import { graphql } from '#core/api/graphql.client';
+
+const GET_CHARACTERS = `
+  query GetCharacters {
+    characters {
+      results {
+        id
+        name
+        status
+        species
+        type
+        gender
+        origin {
+          name
+        }
+        location {
+          name
+        }
+        image
+        episode {
+          id
+          name
+        }
+        created
+      }
+    }
+  }
+`;
+
+interface CharactersResponse {
+  characters: {
+    results: Character[];
+  };
+}
 
 export const getCharacterCollection = async (): Promise<Character[]> => {
-  const response = await fetch('http://localhost:3000/api/character');
-  const data = await response.json();
-  return data.results;
+  const data = await graphql<CharactersResponse>({
+    query: GET_CHARACTERS,
+  });
+  return data.characters.results;
 };
