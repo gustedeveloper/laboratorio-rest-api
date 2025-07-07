3a0c1efeff288c0dbfd88f511d4fa573fc9263c2
Guste Gaubaite <219.guste@gmail.com>
2025-07-07 08:39:46 +0200

    feat: switch from external API to local server endpoints

--- src/pods/character-collection/api/character-collection.api.ts
+++ src/pods/character-collection/api/character-collection.api.ts
@@ -1,7 +1,7 @@
 import { Character } from './character-collection.api-model';
 
 export const getCharacterCollection = async (): Promise<Character[]> => {
-  const response = await fetch('https://rickandmortyapi.com/api/character');
+  const response = await fetch('http://localhost:3000/api/character');
   const data = await response.json();
   return data.results;
 };
--- src/pods/character/api/character.api.ts
+++ src/pods/character/api/character.api.ts
@@ -1,13 +1,19 @@
 import { Character } from './character.api-model';
 
 export const getCharacter = async (id: string): Promise<Character> => {
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character/${id}`
-  );
+  const response = await fetch(`http://localhost:3000/api/character/${id}`);
   const data = await response.json();
   return data;
 };
 
 export const saveCharacter = async (character: Character): Promise<boolean> => {
-  return true;
+  const response = await fetch(
+    `http://localhost:3000/api/character/${character.id}`,
+    {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(character),
+    }
+  );
+  return response.ok;
 };
